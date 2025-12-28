0262afbfe39b4a92846b6d6bf3ef068a7793b29d
Elijah Melton <elijah.m.melton@gmail.com>
2025-12-28 13:41:49 -0800

    style: strip styling to browser defaults

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CreateEvent from './CreateEvent.jsx'
 import EventView from './EventView.jsx'
 import './index.css'
@@ -8,9 +8,6 @@ import './index.css'
 function App() {
   return (
     <BrowserRouter>
-      <nav className="navbar">
-        <Link to="/" className="nav-brand">📅 Schedule</Link>
-      </nav>
       <Routes>
         <Route path="/" element={<CreateEvent />} />
         <Route path="/event/:eventId" element={<EventView />} />
